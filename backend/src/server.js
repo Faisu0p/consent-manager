9b1271cf83e4f7c0b3db0781b131253a1a298e84
Faisu <faisal.khan786hi@gmail.com>
2025-03-04 13:16:32 +0530

    Created Api to fetch the access logs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import userRoute from './routes/userRoutes.js';
+import accessLogRoute from './routes/accessLogRoutes.js';
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(express.json());
 
 // Routes for users
 app.use('/api/users', userRoute);
+
+// Routes for access logs
+app.use("/api/access-logs", accessLogRoute);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
