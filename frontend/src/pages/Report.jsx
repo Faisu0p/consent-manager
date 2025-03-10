72850bf6426481fbe2bc09a9dcfc07a689ab01c3
Faisu <faisal.khan786hi@gmail.com>
2025-03-10 14:37:07 +0530

    Added ConsentTrends to reports page

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/Report.css";
+import ConsentTrends from "../components/ConsentTrends";
 
 const Report = () => {
   return (
     <div className="report-container">
-      <h1>........................................................Welcome to Report & Analytics..............................................</h1>
+      <h1>Welcome to Report & Analytics</h1>
+      <ConsentTrends />
     </div>
   );
 };
