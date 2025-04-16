07815f350e29d1d76f32d29427a8dd9620994692
Faisu <faisal.khan786hi@gmail.com>
2025-04-16 11:30:21 +0530

    styling sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,9 @@
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Link, useNavigate } from "react-router-dom";
-import { FiUsers, FiSettings, FiLogOut, FiBarChart2, FiFileText, FiHeadphones } from "react-icons/fi";
-import { MdManageAccounts, MdDashboard } from "react-icons/md";
-import { RiFileShield2Line } from "react-icons/ri";
-import { TbPencilCog } from "react-icons/tb";
+import { FiUsers, FiSettings, FiLogOut, FiBarChart, FiClipboard, FiSliders, FiEdit3, FiShield, FiHome, FiUserCheck, FiHeadphones } from "react-icons/fi";
 
 import { logout } from "../services/authService";
+
 import "../styles/Sidebar.css";
 
 const SidebarComponent = ({ isCollapsed, toggleSidebar }) => {
@@ -19,22 +17,25 @@ const SidebarComponent = ({ isCollapsed, toggleSidebar }) => {
   return (
     <div className={`sidebar-component ${isCollapsed ? "collapsed" : ""}`}>
       <Sidebar collapsed={isCollapsed}>
-        <div className="logo-container">
-          {!isCollapsed && <h2 className="logo-text">Dashboard</h2>}
-        </div>
         <Menu>
-          <MenuItem icon={<MdDashboard />} component={<Link to="/dashboard" />}>Dashboard</MenuItem>
-          <MenuItem icon={<MdManageAccounts />} component={<Link to="/consents" />}>Consent Management</MenuItem>
-          <MenuItem icon={<FiUsers />} component={<Link to="/users" />}>Users</MenuItem>
-          <MenuItem icon={<FiFileText />} component={<Link to="/audit-logs" />}>Audit Logs</MenuItem>
-          <MenuItem icon={<FiFileText />} component={<Link to="/customization" />}>Customization</MenuItem>
-          <MenuItem icon={<TbPencilCog />} component={<Link to="/modify-banner" />}>Modify Banner</MenuItem>
-          <MenuItem icon={<FiBarChart2 />} component={<Link to="/reports" />}>Reports & Analytics</MenuItem>
-          <MenuItem icon={<RiFileShield2Line />} component={<Link to="/view-consents" />}>View Consents</MenuItem>
-          <MenuItem icon={<FiHeadphones />} component={<Link to="/customer-support" />}>Customer Support</MenuItem>
-          <MenuItem icon={<FiSettings />} component={<Link to="/settings" />}>Settings</MenuItem>
-          <MenuItem icon={<FiLogOut />} onClick={handleLogout} className="logout-item">Logout</MenuItem>
+          <MenuItem icon={<FiHome size={18} />} component={<Link to="/dashboard" />}>Dashboard</MenuItem>
+          <MenuItem icon={<FiUserCheck size={18} />} component={<Link to="/consents" />}>Consent Management</MenuItem>
+          <MenuItem icon={<FiUsers size={18} />} component={<Link to="/users" />}>Users</MenuItem>
+          <MenuItem icon={<FiClipboard size={18} />} component={<Link to="/audit-logs" />}>Audit Logs</MenuItem>
+          <MenuItem icon={<FiSliders size={18} />} component={<Link to="/customization" />}>Customization</MenuItem>
+          <MenuItem icon={<FiEdit3 size={18} />} component={<Link to="/modify-banner" />}>Modify Banner</MenuItem>
+          <MenuItem icon={<FiBarChart size={18} />} component={<Link to="/reports" />}>Reports & Analytics</MenuItem>
+          <MenuItem icon={<FiShield size={18} />} component={<Link to="/view-consents" />}>View Consents</MenuItem>
+          <MenuItem icon={<FiHeadphones size={18} />} component={<Link to="/customer-support" />}>Customer Support</MenuItem>
         </Menu>
+
+        <div className="sidebar-bottom-section">
+          <Menu>
+            <MenuItem icon={<FiSettings size={18} />} component={<Link to="/settings" />}>Settings</MenuItem>
+            <MenuItem icon={<FiLogOut size={18} />} onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
+          <span className="sidebar-version">KONSENTO v1.0</span>
+        </div>
       </Sidebar>
 
       <button className="sidebar-component-toggle-btn" onClick={toggleSidebar}>
@@ -45,52 +46,3 @@ const SidebarComponent = ({ isCollapsed, toggleSidebar }) => {
 };
 
 export default SidebarComponent;
-
-
-
-// import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-// import { Link, useNavigate } from "react-router-dom";
-// import { FiUsers, FiSettings, FiLogOut, FiBarChart2, FiFileText } from "react-icons/fi";
-// import { MdManageAccounts, MdDashboard } from "react-icons/md";
-// import { RiFileShield2Line } from "react-icons/ri";
-// import { TbPencilCog } from "react-icons/tb";
-// import { FiHeadphones } from "react-icons/fi";
-
-// import { logout } from "../services/authService";
-
-// import "../styles/Sidebar.css";
-
-// const SidebarComponent = ({ isCollapsed, toggleSidebar }) => {
-//   const navigate = useNavigate();
-
-//   const handleLogout = async () => {
-//     await logout();
-//     navigate("/");
-//   };
-
-//   return (
-//     <div className={`sidebar-component ${isCollapsed ? "collapsed" : ""}`}>
-//       <Sidebar collapsed={isCollapsed}>
-//         <Menu>
-//           <MenuItem icon={<MdDashboard />} component={<Link to="/dashboard" />}>Dashboard</MenuItem>
-//           <MenuItem icon={<MdManageAccounts />} component={<Link to="/consents" />}>Consent Management</MenuItem>
-//           <MenuItem icon={<FiUsers />} component={<Link to="/users" />}>Users</MenuItem>
-//           <MenuItem icon={<FiFileText />} component={<Link to="/audit-logs" />}>Audit Logs</MenuItem>
-//           <MenuItem icon={<FiFileText />} component={<Link to="/customization" />}>Customization</MenuItem>
-//           <MenuItem icon={<TbPencilCog />} component={<Link to="/modify-banner" />}>Modify Banner</MenuItem>
-//           <MenuItem icon={<FiBarChart2 />} component={<Link to="/reports" />}>Reports & Analytics</MenuItem>
-//           <MenuItem icon={<RiFileShield2Line />} component={<Link to="/view-consents" />}>View Consents</MenuItem>
-//           <MenuItem icon={<FiHeadphones />} component={<Link to="/customer-support" />}>Customer Support</MenuItem>
-//           <MenuItem icon={<FiSettings />} component={<Link to="/settings" />}>Settings</MenuItem>
-//           <MenuItem icon={<FiLogOut />} onClick={handleLogout}>Logout</MenuItem>
-//         </Menu>
-//       </Sidebar>
-
-//       <button className="sidebar-component-toggle-btn" onClick={toggleSidebar}>
-//         {isCollapsed ? "☰" : "✖"}
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default SidebarComponent;
